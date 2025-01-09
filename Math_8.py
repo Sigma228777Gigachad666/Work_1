d5ba134ecf9cd84ff8cfc89ede818fd5352ae18f
Sigma228777Gigachad666 <murad.dukh@gmail.com>
2025-01-09 11:43:22 +0300

    commit

---
+++ Math_8.py
@@ -0,0 +1,18 @@
+x = input()
+
+a = str((int(x) % 10))
+b = str((int(x) % 100) // 10)
+c = str(int(x) // 100)
+
+n = c + a + b 
+s = b + a + c
+w = b + a + c
+z = a + c + b 
+v = a + b + c
+
+print(x)
+print(n)
+print(s)
+print(w)
+print(z)
+print(v)
